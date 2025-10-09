de022a9a556432b8f053bc587400264ef3e698ad
Roshan <roshaniruku@gmail.com>
2025-10-09 18:28:14 -0400

    added stuff

--- website/apps/web/components/footer.tsx
+++ website/apps/web/components/footer.tsx
@@ -76,7 +76,7 @@ export function Footer(props?: FooterProps) {
   } = props || {};
 
   return (
-    <footer className="bg-fblc-dark-gray text-white">
+    <footer className="bg-gray-900 text-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Main Footer Content */}
         <div className="py-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8">
