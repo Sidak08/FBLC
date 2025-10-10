6752db71d104fb15b0cfdb0c780b4f6938209bf6
Roshan <roshaniruku@gmail.com>
2025-10-09 21:48:55 -0400

    components

---
+++ website/apps/web/components/governance/index.ts
@@ -0,0 +1,3 @@
+export { default as HeroSection } from './hero-section'
+export { default as GovernanceCard } from './governance-card'
+export { default as GovernanceStructureSection } from './governance-structure-section'
